c10998fd26ebc45601e00d60f7d2bbfc2ba73898
Roshni Gawade <roshnigawade14@gmail.com>
2024-02-20 10:23:30 +0530

    Initial commit

---
+++ src/main/java/com/CalculatorApp/CalculatorAppApplication.java
@@ -0,0 +1,15 @@
+package com.CalculatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CalculatorAppApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
